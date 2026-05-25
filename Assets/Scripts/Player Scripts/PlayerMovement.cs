6bb4952943186de2648c52a26d4c406c84507a51
KoharuLover <shadowthe097@gmail.com>
2026-05-25 17:22:03 +0800

    Working Player Movement and Animation

--- Assets/Scripts/Player Scripts/PlayerMovement.cs
+++ Assets/Scripts/Player Scripts/PlayerMovement.cs
@@ -7,10 +7,17 @@ public class PlayerMovement : MonoBehaviour
     private Vector2 _movement;
 
     private Rigidbody2D _rb;
+    private Animator _animator;
+
+    private const string _horizontal = "Horizontal";
+    private const string _vertical = "Vertical";
+    private const string _lastHorizontal = "LastHorizontal";
+    private const string _lastVertical = "LastVertical";
 
     private void Awake()
     {
         _rb = GetComponent<Rigidbody2D>();
+        _animator = GetComponent<Animator>();
     }
 
     private void Update()
@@ -18,5 +25,14 @@ public class PlayerMovement : MonoBehaviour
         _movement.Set(InputManager.Movement.x, InputManager.Movement.y);
 
         _rb.linearVelocity = _movement * _moveSpeed;
+
+        _animator.SetFloat(_horizontal, _movement.x);
+        _animator.SetFloat(_vertical, _movement.y);
+
+        if (_movement != Vector2.zero)
+        {
+            _animator.SetFloat(_lastHorizontal, _movement.x);
+            _animator.SetFloat(_lastVertical, _movement.y);
+        }
     }
 }
