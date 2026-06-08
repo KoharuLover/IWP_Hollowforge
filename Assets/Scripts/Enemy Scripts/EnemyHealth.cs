c7089d12b26294df10aeeee3acd6f20f9b43a72c
KoharuLover <shadowthe097@gmail.com>
2026-06-08 18:00:00 +0800

    Enemy Death and Hit Effects (also other bugfixes)
    - increased attack range so the skeleton can actually hit player faster

--- Assets/Scripts/Enemy Scripts/EnemyHealth.cs
+++ Assets/Scripts/Enemy Scripts/EnemyHealth.cs
@@ -1,11 +1,27 @@
+using System.Collections;
 using UnityEngine;
 
 public class EnemyHealth : MonoBehaviour
 {
     private EnemyStats _enemyStats;
+    private Animator _animator;
+    private Rigidbody2D _rb;
+    private Collider2D _collider;
+    private SpriteRenderer _spriteRenderer;
 
     private float _currentHealth;
     private bool _isDead;
+
+    private Color _originalColor;
+    private Coroutine _flashCoroutine;
+
+    private const string IsDeadParameter = "IsDead";
+    private const string IsMovingParameter = "IsMoving";
+    private const string IsAttackingParameter = "IsAttacking";
+
+    [Header("Hit Flash")]
+    [SerializeField] private Color _flashColor = Color.red;
+    [SerializeField] private float _flashDuration = 0.1f;
 
     public float CurrentHealth => _currentHealth;
     public float MaxHealth => _enemyStats.MaxHealth;
@@ -14,11 +30,20 @@ public class EnemyHealth : MonoBehaviour
     private void Awake()
     {
         _enemyStats = GetComponent<EnemyStats>();
+        _animator = GetComponent<Animator>();
+        _rb = GetComponent<Rigidbody2D>();
+        _collider = GetComponent<Collider2D>();
+        _spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     private void Start()
     {
         _currentHealth = MaxHealth;
+
+        if (_spriteRenderer != null)
+        {
+            _originalColor = _spriteRenderer.color;
+        }
     }
 
     public void TakeDamage(float damageAmount, ElementType damageElement)
@@ -33,12 +58,15 @@ public class EnemyHealth : MonoBehaviour
         _currentHealth -= finalDamage;
         _currentHealth = Mathf.Clamp(_currentHealth, 0f, MaxHealth);
 
-        Debug.Log(gameObject.name + " took " + finalDamage + " damage. HP: " + _currentHealth);
+        Debug.Log(gameObject.name + " took " + finalDamage + " " + damageElement + " damage. HP: " + _currentHealth);
 
         if (_currentHealth <= 0f)
         {
             Die();
+            return;
         }
+
+        PlayHitFlash();
     }
 
     private float CalculateElementalDamage(float damageAmount, ElementType damageElement)
@@ -56,6 +84,35 @@ public class EnemyHealth : MonoBehaviour
         return damageAmount;
     }
 
+    private void PlayHitFlash()
+    {
+        if (_spriteRenderer == null)
+        {
+            return;
+        }
+
+        if (_flashCoroutine != null)
+        {
+            StopCoroutine(_flashCoroutine);
+        }
+
+        _flashCoroutine = StartCoroutine(HitFlashRoutine());
+    }
+
+    private IEnumerator HitFlashRoutine()
+    {
+        _spriteRenderer.color = _flashColor;
+
+        yield return new WaitForSeconds(_flashDuration);
+
+        if (_spriteRenderer != null && _isDead == false)
+        {
+            _spriteRenderer.color = _originalColor;
+        }
+
+        _flashCoroutine = null;
+    }
+
     private void Die()
     {
         if (_isDead)
@@ -65,16 +122,39 @@ public class EnemyHealth : MonoBehaviour
 
         _isDead = true;
 
-        Debug.Log(gameObject.name + " died");
+        if (_flashCoroutine != null)
+        {
+            StopCoroutine(_flashCoroutine);
+        }
 
+        if (_spriteRenderer != null)
+        {
+            _spriteRenderer.color = _originalColor;
+        }
+
+        if (_rb != null)
+        {
+            _rb.linearVelocity = Vector2.zero;
+        }
+
+        if (_collider != null)
+        {
+            _collider.enabled = false;
+        }
+
+        if (_animator != null)
+        {
+            _animator.SetBool(IsMovingParameter, false);
+            _animator.SetBool(IsAttackingParameter, false);
+            _animator.SetBool(IsDeadParameter, true);
+        }
+
+        Debug.Log(gameObject.name + " died");
+    }
+
+    // Animation Event: put this at the end of the death animation.
+    public void DestroyEnemy()
+    {
         Destroy(gameObject);
     }
-    private void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.K))
-        {
-            TakeDamage(10f, ElementType.Physical);
-        }
-    }
-
 }
